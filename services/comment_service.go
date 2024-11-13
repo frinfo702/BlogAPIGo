d56d4aa781238a9ffa330b904cc7e028ac8c03e3
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2024-11-13 11:48:14 +0900

    fixed typo

--- services/comment_service.go
+++ services/comment_service.go
@@ -10,11 +10,11 @@ import (
 // ハンドラ層が Comment 構造体関連で呼び出したい処理
 // PostCommenthandlerでの使用を想定
 func (s *MyappService) PostCommentService(comment models.Comment) (models.Comment, error) {
-	insetedComment, err := repositories.InsertComment(s.db, comment)
+	insertedComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
 		log.Printf("failed to insert new comment: %v", err)
 		return comment, err
 	}
 
-	return insetedComment, nil
+	return insertedComment, nil
 }
